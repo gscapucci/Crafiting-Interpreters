5d2ce4ea157aa9b9204f9831c228916ac340c21a
Guilherme Capucci <guilherme_capucci@outlook.com>
2024-09-15 02:03:13 -0300

    Chapter 18.4

--- vm.c
+++ vm.c
@@ -16,6 +16,10 @@ static Value peek(int distance) {
     return vm.stack_top[-1 - distance];
 }
 
+static bool is_falsey(Value value) {
+    return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
 static void runtime_error(const char *format, ...) {
     va_list args;
     va_start(args, format);
@@ -69,10 +73,24 @@ static InterpretResult run() {
                 push(constant);
                 break;
             }
+            case OP_NIL: push(NIL_VAL); break;
+            case OP_TRUE: push(BOOL_VAL(true)); break;
+            case OP_FALSE: push(BOOL_VAL(false)); break;
+            case OP_EQUAL: {
+                Value b = pop();
+                Value a = pop();
+                push(BOOL_VAL(values_equal(a, b)));
+                break;
+            }
+            case OP_GREATER:    BINARY_OP(BOOL_VAL, >); break;
+            case OP_LESS:       BINARY_OP(BOOL_VAL, <); break;
             case OP_ADD:        BINARY_OP(NUMBER_VAL, +); break;
             case OP_SUBTRACT:   BINARY_OP(NUMBER_VAL, -); break;
             case OP_MULTIPLY:   BINARY_OP(NUMBER_VAL, *); break;
             case OP_DIVIDE:     BINARY_OP(NUMBER_VAL, /); break;
+            case OP_NOT:
+                push(BOOL_VAL(is_falsey(pop())));
+                break;
             case OP_NEGATE: 
                 if(!IS_NUMBER(peek(0))) {
                     runtime_error("Operand must be a number.");
